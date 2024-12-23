5a05737fcb0308c79d85830be788f6dbde4c26d4
Dmytro <dowlandmemes@gmail.com>
2024-12-23 18:25:19 +0200

    login page + check if match from json

--- src/api/login.py
+++ src/api/login.py
@@ -1,7 +1,50 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Form
+from fastapi.responses import HTMLResponse
+import json
 
 router = APIRouter(tags=["login page"])
 
-@router.get('/login')
+@router.get('/login', response_class=HTMLResponse)
 def login():
-    return {"value": "success"}
+    html_content = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Login Page</title>
+    </head>
+    <body>
+        <h1>Login</h1>
+        <form action="/submit-login" method="post">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+            <br><br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+            <br><br>
+            <button type="submit">підтвердити</button>
+        </form>
+    </body>
+    </html>
+    """
+    return html_content
+
+
+def check_login(password, login):
+    with open("/home/dmytro/tap_1/TAPproject/src/api/credentials.json", "r") as file:
+        content = json.load(file)
+    try:
+        if str(password) != str(content["users"][str(login)]["password"]):
+     
+            return "wrong password or login"
+        else:
+            return "success"
+    except KeyError:
+        return "wrong password or login"
+
+@router.post('/submit-login')
+def submit_login(username: str = Form(...), password: str = Form(...)):
+    return check_login(password, username)
+    
+
+
+    
