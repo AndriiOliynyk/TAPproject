a118f6af9cc5b031001be28bdefcf7c1b5a91a1c
AndriiOliynyk <andrijolijnik462@gmail.com>
2024-12-23 14:15:32 +0000

    tap_v1_html

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Form
+from fastapi.responses import HTMLResponse
+from typing import Dict
+from pydantic import BaseModel
 
 app = FastAPI()
 
+locations = ["Площа Ринок", "Оперний театр", "Високий замок"]
+rates = {
+    10: 25,
+    15: 35,
+    25: 40
+}
+
+class RentalRequest(BaseModel):
+    location: str
+    duration: int
 
 @app.get("/")
 def read_root():
-    return {"Hello": "by2e"}
+    return {"Hello": "World"}
